9d26249d85f63861020a534f8d49b9fbe7add85a
Paul2D <paul.maxineanu@gmail.com>
2022-09-14 11:35:34 +0300

    build for production

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# covid19-statistics
+# Covid19 Statistics app using vuejs and tailwindcss
 
 This app use the covid api endpoint from covid19api.com
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/covid_statistics_app_vuejs_tailwindcss/" : "/",
 })
